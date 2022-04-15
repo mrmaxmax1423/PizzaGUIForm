a555c9d1e1f83ecb851ad1c2cc49de41c841b6df
mrmaxmax1423 <maximilianods@ymail.com>
2022-04-15 18:11:32 -0400

    Initial commit

---
+++ src/PizzaGUIFrame.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class PizzaGUIFrame {
+    JPanel mainPanel;
+
+    JPanel optionPanel;
+    JRadioButton crustSelectRB;
+    JComboBox sizeSelectCB;
+    JCheckBox toppingSelectChB;
+
+    JPanel orderDisplayPanel;
+    JTextArea orderTextArea;
+
+    JPanel controlPanel;
+    JButton quitButton;
+    JButton quitButton;
+    JButton quitButton;
+}
